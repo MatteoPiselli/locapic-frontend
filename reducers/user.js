181299e0f1258c305fc4c782cb1ec87017ee851a
Matteo <pro.matteo.piselli@gmail.com>
2025-05-15 18:45:13 +0200

    locapic front end

---
+++ reducers/user.js
@@ -0,0 +1,30 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  value: { nickname: null, places: [] },
+};
+
+export const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {
+    updateNickname: (state, action) => {
+      state.value.nickname = action.payload;
+    },
+    addPlace: (state, action) => {
+      state.value.places.push(action.payload);
+    },
+    removePlace: (state, action) => {
+      state.value.places = state.value.places.filter(
+        (e) => e.name !== action.payload
+      );
+    },
+    showAllPlaces: (state, action) => {
+      state.value.places = action.payload;
+    },
+  },
+});
+
+export const { updateNickname, addPlace, removePlace, showAllPlaces } =
+  userSlice.actions;
+export default userSlice.reducer;
